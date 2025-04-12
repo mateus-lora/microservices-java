f8c66317e524fa6955052080bd56658879b50144
Mateus Lora <loramateusatitus@gmail.com>
2025-04-11 21:01:55 -0300

    Update project

--- greating-service/src/main/java/br/edu/atitus/greatingservice/GreatingController.java
+++ greating-service/src/main/java/br/edu/atitus/greatingservice/GreatingController.java
@@ -8,25 +8,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.atitus.greatingservice.configs.GreatingConfig;
+
 @RestController
 @RequestMapping("/greating-service")
 public class GreatingController {
 	
-	@Value("${greating-service.greating}")
-	private String greating;
+//	@Value("${greating-service.greating}")
+//	private String greating;
+//	
+//	@Value("${greating-service.default-name}")
+//	private String defaultName;
 	
-	@Value("${greating-service.default-name}")
-	private String defaultName;
+	private final GreatingConfig greatingConfig;
 	
+	public GreatingController(GreatingConfig greatingConfig) {
+	super();
+	this.greatingConfig = greatingConfig;
+}
+
 	@GetMapping({"","/", "/{namePath}"})
 	public ResponseEntity<String> getGreating(
 			@PathVariable(required = false) String namePath,
 			@RequestParam(required = false) String name
 			) {
 		if (name == null)
-			name = namePath != null ? namePath : defaultName;
+			name = namePath != null ? namePath : greatingConfig.getDefaultName();
 		
-		String returnText = String.format("%s %s!",greating, name);
+		String returnText = String.format("%s %s!", greatingConfig.getGreating(), name);
 		return ResponseEntity.ok(returnText);
 	}
 

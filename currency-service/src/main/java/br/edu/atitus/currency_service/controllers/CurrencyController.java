cd925d147c437d3bc0c90adea7f8cf2b9db8777e
Mateus Lora <loramateusatitus@gmail.com>
2025-05-10 23:14:37 -0300

    feat(currency-service): Adiciona novo microserviço para conversão de moedas

---
+++ currency-service/src/main/java/br/edu/atitus/currency_service/controllers/CurrencyController.java
@@ -0,0 +1,45 @@
+package br.edu.atitus.currency_service.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.atitus.currency_service.entities.CurrencyEntity;
+import br.edu.atitus.currency_service.repositories.CurrencyRepository;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("currency")
+public class CurrencyController {
+
+	private final CurrencyRepository repository;
+
+	public CurrencyController(CurrencyRepository repository) {
+		super();
+		this.repository = repository;
+	}
+	
+	@Value("${server.port}")
+	private int serverPort;
+	
+	@GetMapping("/{value}/{source}/{target}")
+	public ResponseEntity<CurrencyEntity> getCurrency(
+			@PathVariable double value,
+			@PathVariable String source,
+			@PathVariable String target
+			) throws Exception{
+		CurrencyEntity currency = repository.findBySourceAndTarget(source, target)
+				.orElseThrow(() -> new Exception("Currency Unsupported"));
+		
+		currency.setConvertedValue(value * currency.getConversionRate());
+		currency.setEnviroment("Currency-service running on port: " + serverPort);
+		
+		return ResponseEntity.ok(currency);
+	}
+	
+}

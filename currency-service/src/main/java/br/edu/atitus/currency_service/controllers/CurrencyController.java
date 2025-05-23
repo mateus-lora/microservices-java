9c8d578876da3c15f18fec068c846a1373b0ba9a
Mateus Lora <loramateusatitus@gmail.com>
2025-05-23 00:04:18 -0300

    feat(currency-service): integra API BCB (Feign) para conversão de moeda

--- currency-service/src/main/java/br/edu/atitus/currency_service/controllers/CurrencyController.java
+++ currency-service/src/main/java/br/edu/atitus/currency_service/controllers/CurrencyController.java
@@ -1,43 +1,71 @@
 package br.edu.atitus.currency_service.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import br.edu.atitus.currency_service.entities.CurrencyEntity;
-import br.edu.atitus.currency_service.repositories.CurrencyRepository;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.atitus.currency_service.clients.CurrencyBCClient;
+import br.edu.atitus.currency_service.clients.CurrencyBCResponse;
+import br.edu.atitus.currency_service.entities.CurrencyEntity;
+import br.edu.atitus.currency_service.repositories.CurrencyRepository;
 
 @RestController
-@RequestMapping("currency")
+@RequestMapping ("currency")
 public class CurrencyController {
 
 	private final CurrencyRepository repository;
+	private final CurrencyBCClient currencyBCClient;
 
-	public CurrencyController(CurrencyRepository repository) {
+	public CurrencyController(CurrencyRepository repository, CurrencyBCClient currencyBCClient) {
 		super();
 		this.repository = repository;
+		this.currencyBCClient = currencyBCClient;
 	}
 	
-	@Value("${server.port}")
+	@Value ("${server.port}")
 	private int serverPort;
 	
-	@GetMapping("/{value}/{source}/{target}")
-	public ResponseEntity<CurrencyEntity> getCurrency(
+	@GetMapping ("/{value}/{source}/{target}")
+	public ResponseEntity <CurrencyEntity> getCurrency(
 			@PathVariable double value,
 			@PathVariable String source,
 			@PathVariable String target
 			) throws Exception{
-		CurrencyEntity currency = repository.findBySourceAndTarget(source, target)
-				.orElseThrow(() -> new Exception("Currency Unsupported"));
+//		CurrencyEntity currency = repository.findBySourceAndTarget(source, target)
+//				.orElseThrow(() -> new Exception("Currency Unsupported"));
+		
+		source = source.toUpperCase();
+		target = target.toUpperCase();
+		String dataSource = "None";
+		
+		CurrencyEntity currency = new CurrencyEntity();
+		currency.setSource(source);
+		currency.setTarget(target);
+		
+		if (source.equals(target)) {
+			currency.setConversionRate(1);
+		} else {
+			double currencySource = 1;
+			double currencyTarget = 1;
+			if (!source.equals("BRL")) {
+				CurrencyBCResponse response = currencyBCClient.getCurrency(source);
+				if (response.getValue().isEmpty()) throw new Exception("Currency not found for " + source);
+				currencySource = response.getValue().get(0).getCotacaoVenda();
+			}
+			if (!target.equals("BRL")) {
+				CurrencyBCResponse response = currencyBCClient.getCurrency(target);
+				if (response.getValue().isEmpty()) throw new Exception("Currency not found for " + target);
+				currencyTarget = response.getValue().get(0).getCotacaoVenda();
+			}
+			currency.setConversionRate(currencySource / currencyTarget);
+			dataSource = "API BCB";
+		}
 		
 		currency.setConvertedValue(value * currency.getConversionRate());
-		currency.setEnviroment("Currency-service running on port: " + serverPort);
+		currency.setEnviroment("Currency-service running on port: " + serverPort + " - DataSource: " + dataSource);
 		
 		return ResponseEntity.ok(currency);
 	}

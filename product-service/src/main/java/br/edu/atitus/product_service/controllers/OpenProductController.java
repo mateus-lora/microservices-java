0d2749148a2da44f3465c3ab6ee55e837949111b
Mateus Lora <loramateusatitus@gmail.com>
2025-05-25 21:28:11 -0300

    feat(product-service): Implementando Cache com Caffeine

--- product-service/src/main/java/br/edu/atitus/product_service/controllers/OpenProductController.java
+++ product-service/src/main/java/br/edu/atitus/product_service/controllers/OpenProductController.java
@@ -1,6 +1,7 @@
 package br.edu.atitus.product_service.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Fallback;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,11 +20,13 @@ public class OpenProductController {
 	
 	private final ProductRepository repository;
 	private final CurrencyClient currencyClient;
+	private final CacheManager cacheManager;
 
-	public OpenProductController(ProductRepository repository, CurrencyClient currencyClient) {
+	public OpenProductController(ProductRepository repository, CurrencyClient currencyClient, CacheManager cacheManager) {
 		super();
 		this.repository = repository;
 		this.currencyClient = currencyClient;
+		this.cacheManager = cacheManager;
 	}
 	
 	@Value("${server.port}")
@@ -34,23 +37,37 @@ public class OpenProductController {
 			@PathVariable Long idProduct,
 			@PathVariable String targetCurrency
 			) throws Exception{
-		ProductEntity product = repository.findById(idProduct)
-				.orElseThrow(() -> new Exception("Product Unsupported"));
 		
-		product.setEnvironment("Product-service running on port: " + serverPort);
-
-		if (product.getCurrency().equals(targetCurrency)) {
-			product.setConvertedPrice(product.getPrice());
+		String nameCache = "Product";
+		String keyCache = idProduct + targetCurrency;
+		
+		ProductEntity product = cacheManager.getCache(nameCache).get(keyCache, ProductEntity.class);
+		
+        String baseEnvironment = "Product-service running on port: " + serverPort;
+		
+		if (product != null) {
+            product.setEnvironment(baseEnvironment + " - Origin: Cache");
 		} else {
-			CurrencyResponse currency = currencyClient.getCurrency(product.getPrice(),product.getCurrency(),targetCurrency);
-			if (currency.getConvertedValue() != -1) {
-				product.setConvertedPrice(currency.getConvertedValue());
-				product.setEnvironment(product.getEnvironment() + " - " + currency.getEnviroment());
-			} else {
+			product = repository.findById(idProduct)
+                    .orElseThrow(() -> new Exception("Product Unsupported"));
+			if (product.getCurrency().equals(targetCurrency)) {
 				product.setConvertedPrice(product.getPrice());
+                product.setEnvironment(baseEnvironment + " - No conversion needed");
+				cacheManager.getCache(nameCache).put(keyCache, product);
+			} else {
+				CurrencyResponse currency = currencyClient.getCurrency(product.getPrice(),product.getCurrency(),targetCurrency);
+				if (currency.getConvertedValue() != -1) {
+					product.setConvertedPrice(currency.getConvertedValue());
+                    product.setEnvironment(baseEnvironment + " - Converted by " + currency.getEnviroment());
+    				cacheManager.getCache(nameCache).put(keyCache, product);
+				} else {
+					product.setConvertedPrice(product.getPrice());
+                    product.setEnvironment(baseEnvironment + " - Conversion Failed/Fallback");
+				}
+				
 			}
-
 		}
+		
 		return ResponseEntity.ok(product);
 	}
 
